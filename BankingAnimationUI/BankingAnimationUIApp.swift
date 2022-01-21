dd1812d387766d7d628934ff16baaea9a496d3c5
Quang Bao <hoangquangbao.developer@gmail.com>
2022-01-21 09:12:18 +0700

    Initial Commit

---
+++ BankingAnimationUI/BankingAnimationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAnimationUIApp.swift
+//  BankingAnimationUI
+//
+//  Created by Quang Bao on 21/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BankingAnimationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
