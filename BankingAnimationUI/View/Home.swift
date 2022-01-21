7c5d1c3baec9ad22db7cfabd0058b4d1df6fd401
Quang Bao <hoangquangbao.developer@gmail.com>
2022-01-21 15:57:33 +0700

    Credit Card UI

--- BankingAnimationUI/View/Home.swift
+++ BankingAnimationUI/View/Home.swift
@@ -52,10 +52,111 @@ struct Home: View {
             }
             .padding()
             
+            CreditCard()
+            
         }
         .vTop()
         .hCenter()
         .background(Color("BG"))
+    }
+    
+    // MARK: Animated Credit Card
+    @ViewBuilder
+    func CreditCard() -> some View {
+        
+        ZStack{
+
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color("Pink"))
+            
+            VStack{
+                
+                HStack{
+                    ForEach(1...4,id: \.self){_ in
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 5, height: 5)
+                    }
+                    
+                    Text("7864")
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                }
+                .hLeading()
+                
+                HStack(spacing: -12){
+                    
+                    Text("Hoang Quang Bao")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .hLeading()
+                    
+                    Circle()
+                        .stroke(.white, lineWidth: 1)
+                        .frame(width: 30, height: 30)
+                    
+                    Circle()
+                        .stroke(.white, lineWidth: 1)
+                        .frame(width: 30, height: 30)
+                }
+                .vBottom()
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal)
+            .vTop()
+            //.hLeading()
+            
+            Circle()
+                .stroke(.white.opacity(0.5),lineWidth: 15)
+                .offset(x: 150, y: -150)
+        }
+        .clipped()
+        .frame(height: 250)
+        .padding()
+        
+//        ZStack{
+//
+//            RoundedRectangle(cornerRadius: 15)
+//                .fill(Color("Pink"))
+//                .frame(height: 250)
+//
+//            VStack{
+//
+//                HStack{
+//
+//                    Text("7864")
+//                        .font(.caption2)
+//                        .foregroundColor(.white)
+//
+//                    Circle()
+//                        .stroke(.white, lineWidth: 10)
+//                        .frame(width: 200, height: 200)
+//                        .offset(x: 100, y: -100)
+//                }
+//
+//                HStack{
+//                    Text("Jenna Ezarik")
+//                        .foregroundColor(.white)
+//                        .font(.body)
+//
+//                    Spacer()
+//
+//                    Circle()
+//                        .stroke(.white, lineWidth: 2)
+//                        .frame(width: 30, height: 30)
+//
+//                    Circle()
+//                        .stroke(.white, lineWidth: 2)
+//                        .frame(width: 30, height: 30)
+//                        .offset(x: -20)
+//
+//                }
+//            }
+//        }
+//        .clipped()
+//        .padding()
+//        .hCenter()
     }
 }
 
